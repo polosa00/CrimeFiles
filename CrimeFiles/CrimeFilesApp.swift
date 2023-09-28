f63cd139b61ec11692d270afb5300dea7d464744
Igor Rukaleev <575714470@mail.ru>
2023-09-28 18:57:08 +0300

    adding model, viewModel, correct main view delete persistence delete content view

--- CrimeFiles/CrimeFilesApp.swift
+++ CrimeFiles/CrimeFilesApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct CrimeFilesApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
         }
     }
 }
---
+++ CrimeFiles/Models/Inspection.swift
@@ -0,0 +1,21 @@
+//
+//  Inspection.swift
+//  CrimeFiles
+//
+//  Created by Igor on 28.09.2023.
+//
+
+import SwiftUI
+
+struct Inspection {
+    let title: String
+    let imageName: String
+    let rows: [RowInspection]
+}
+
+struct RowInspection {
+    let title: String
+    let imageName: String
+}
+
+
