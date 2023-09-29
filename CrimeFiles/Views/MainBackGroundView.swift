bfb044fcda48eb301692f0637df3985c480d7d84
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-09-29 15:53:49 +0300

    Add MainBackgroundView, add fonts, delete white frame around list

---
+++ CrimeFiles/Views/MainBackGroundView.swift
@@ -0,0 +1,56 @@
+//
+//  MainBackGroundView.swift
+//  CrimeFiles
+//
+//  Created by Kate Kashko on 29.09.2023.
+//
+
+import SwiftUI
+
+struct MainBackGroundView: View {
+    var body: some View {
+        ZStack{
+            BackgroundView()
+            VStack{
+                HStack{
+                    Image("clip")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: UIScreen.main.bounds.size.width / 10)
+                    
+                    Spacer()
+                    Image("topSecret")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: UIScreen.main.bounds.size.width / 3)
+                        .opacity(0.6)
+                        .rotationEffect(Angle(degrees: 8))
+                        .padding(.top, -15)
+                    
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            VStack{
+                Image("CrimeFiles")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: UIScreen.main.bounds.size.width / 1.6)
+                    .padding(.top, 80)
+                Spacer()
+                Image("detective")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: UIScreen.main.bounds.size.width / 4)
+                    .padding(.bottom, 20)
+                
+            }
+        }
+    }
+}
+
+struct MainBackGroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainBackGroundView()
+    }
+}
